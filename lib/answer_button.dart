ef069e3d3723461e693b9a56ae3002f96cb391f0
armbaby54 <armbaby54@gmail.com>
2025-01-15 09:17:58 +0000

    commit

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
 class AnswerBuntton extends StatelessWidget {
-  const AnswerBuntton(this.answerOption,{super.key});
+  // const AnswerBuntton(this.answerOption,{super.key});
+  const AnswerBuntton({
+    super.key,
+    required this.answerOption,
+    required this.onTap,
+  });
 
   final String answerOption;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
-        foregroundColor: Colors.white,
-      ),
-      child: Text(answerOption),
-      
+
+    return Column(
+      children: [
+        const SizedBox(height: 10,),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: onTap,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+              foregroundColor: Colors.white,
+            ),
+            child: Text(answerOption),
+            
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/answer_button.dart';
-
 import 'package:myapp/data/quizz.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/results_screen.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,10 +14,30 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-  final currentQuestion = questions[0];
+  int currentQuestionIndex = 0;
+
+  final List<String> selectedAnswers = [];
+
+  void answerQuestion(String selectedAnswer) {
+    setState(() {
+      selectedAnswers.add(selectedAnswer);
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => 
+              ResultsScreen(selectedAnswers: selectedAnswers),
+          ),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -29,16 +50,23 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             child: Container(
               margin: const EdgeInsets.all(40),
               child: Column(
-                mainAxisSize: MainAxisSize.min,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     currentQuestion.question,
-                    style: TextStyle(color: Colors.white),
+                    style: GoogleFonts.lato(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                     textAlign: TextAlign.center,
                   ),
                   const SizedBox(height: 30),
-                  ...currentQuestion.answers.map((answer) {
-                    return AnswerBuntton(answer);
+                  ...currentQuestion.getShuffledAnswers().map((answer) {
+                    return AnswerBuntton(
+                        answerOption: answer,
+                        onTap: () => answerQuestion(answer));
                   }),
                 ],
               ),
